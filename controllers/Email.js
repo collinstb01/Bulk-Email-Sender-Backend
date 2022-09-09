4538cd9b123397b2de787a808b4e3e12b9281398
Jayden <collinstb01@gmail.com>
2022-09-09 22:17:41 +0100

    make it better

--- controllers/Email.js
+++ controllers/Email.js
@@ -11,7 +11,7 @@ const login = async (req, res) => {
   console.log(process.env.LOGIN__PASSWORD);
   try {
     if (
-      emailPassword !== "Edewisteve1234" &&
+      emailPassword !== "Steve1234" &&
       emailAddress !== "Officefile@gmail.com"
     ) {
       return res.status(400).json({ message: "Invalid Credentials" });
